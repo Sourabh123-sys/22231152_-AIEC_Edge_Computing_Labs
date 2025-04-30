4072c443026d6c98af6e7bed72570825c3dd77ae
Sourabh Sanjay karmakar <sourabhkarmakar88@gmail.com>
2025-04-30 18:09:41 +0530

    Add files via upload

--- Lab-1/Lab-1/Code1_LED_Blink.py
+++ Lab-1/Lab-1/Code1_LED_Blink.py
@@ -1,1 +1,17 @@
+# Lab_1_GPIO_LED/LED_Blink_BCM
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)  # GPIO4 (Pin 7)
+
+try:
+    while True:
+        GPIO.output(4, True)  # LED ON
+        print("LED ON")
+        time.sleep(1)
+        GPIO.output(4, False)  # LED OFF
+        print("LED OFF")
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
